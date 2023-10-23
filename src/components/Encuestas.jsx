3749448f521cb3f796ee2538fc32ea5a786b9149
Vanessa <vanefratter@mi.unc.edu.ar>
2023-10-23 19:07:06 -0300

    listo

---
+++ src/components/Encuestas.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Encuestas({ encuestas }) {
+  return (
+    <div>
+      <h2>Encuestas Disponibles</h2>
+      <ul>
+        {encuestas.map((encuesta) => (
+          <li key={encuesta.id}>
+            {encuesta.pregunta}
+            <ul>
+            {encuesta.opciones.map((opcion) => (
+              <li key={opcion}>{opcion}</li>
+            ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Encuestas;
